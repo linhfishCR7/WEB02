73bb180a52e3bb405598a4ac80fe33194d2feb0f
linhfishCR7 <59117535+linhfishCR7@users.noreply.github.com>
2020-06-14 00:14:49 +0700

    hình sản phẩm và chủ đề góp ý

---
+++ backend/chudegopy/create.php
@@ -0,0 +1,33 @@
+<?php
+// Include file cấu hình ban đầu của `Twig`
+require_once __DIR__.'/../../bootstrap.php';
+
+// Truy vấn database
+// 1. Include file cấu hình kết nối đến database, khởi tạo kết nối $conn
+include_once(__DIR__.'/../../dbconnect.php');
+
+// 2. Nếu người dùng có bấm nút Đăng ký thì thực thi câu lệnh UPDATE
+if(isset($_POST['btnSave'])) 
+{
+    // Lấy dữ liệu người dùng hiệu chỉnh gởi từ REQUEST POST
+    $tenChuDe = $_POST['cdgy_ten'];
+
+    // Câu lệnh INSERT
+    $sql = "INSERT INTO `chudegopy` (cdgy_ten) VALUES ('" . $tenChuDe . "');";
+
+    // Thực thi INSERT
+    mysqli_query($conn, $sql);
+
+    // Đóng kết nối
+    mysqli_close($conn);
+
+    // Sau khi cập nhật dữ liệu, tự động điều hướng về trang Danh sách
+    header('location:index.php');
+}if(isset($_POST['btnCancel'])) {
+    header('location:index.php');
+
+}
+
+
+// Yêu cầu `Twig` vẽ giao diện được viết trong file `backend/loaisanpham/create.html.twig`
+echo $twig->render('backend/chudegopy/create.html.twig');
---
+++ backend/chudegopy/edit.php
@@ -0,0 +1,42 @@
+<?php
+// Include file cấu hình ban đầu của `Twig`
+require_once __DIR__.'/../../bootstrap.php';
+
+// Truy vấn database
+// 1. Include file cấu hình kết nối đến database, khởi tạo kết nối $conn
+include_once(__DIR__.'/../../dbconnect.php');
+
+// 2. Chuẩn bị câu truy vấn $sqlSelect, lấy dữ liệu ban đầu của record cần update
+// Lấy giá trị khóa chính được truyền theo dạng QueryString Parameter key1=value1&key2=value2...
+$cdgy_ma = $_GET['cdgy_ma'];
+$sqlSelect = "SELECT * FROM `chudegopy` WHERE cdgy_ma=$cdgy_ma;";
+
+// 3. Thực thi câu truy vấn SQL để lấy về dữ liệu ban đầu của record cần update
+$resultSelect = mysqli_query($conn, $sqlSelect);
+$chudegopyRow = mysqli_fetch_array($resultSelect, MYSQLI_ASSOC); // 1 record
+
+// 4. Nếu người dùng có bấm nút Đăng ký thì thực thi câu lệnh UPDATE
+if(isset($_POST['btnSave'])) 
+{
+    // Lấy dữ liệu người dùng hiệu chỉnh gởi từ REQUEST POST
+    $tenChuDe = $_POST['cdgy_ten'];
+
+    // Câu lệnh UPDATE
+    $sql = "UPDATE `chudegopy` SET cdgy_ten='$tenChuDe' WHERE cdgy_ma=$cdgy_ma;";
+
+    // Thực thi UPDATE
+    mysqli_query($conn, $sql);
+
+    // Đóng kết nối
+    mysqli_close($conn);
+
+    // Sau khi cập nhật dữ liệu, tự động điều hướng về trang Danh sách
+    header('location:index.php');
+}if(isset($_POST['btnCancel'])) {
+    header('location:index.php');
+
+}
+
+// Yêu cầu `Twig` vẽ giao diện được viết trong file `backend/loaisanpham/edit.html.twig`
+// với dữ liệu truyền vào file giao diện được đặt tên là `loaisanpham`
+echo $twig->render('backend/chudegopy/edit.html.twig', ['chudegopy' => $chudegopyRow] );
