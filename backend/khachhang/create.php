8160633e26bd2fbc9e42438bf3a7754aabd4cb07
linhfishCR7 <59117535+linhfishCR7@users.noreply.github.com>
2020-06-25 13:20:12 +0700

    hieu chinh index frontend

--- backend/khachhang/create.php
+++ backend/khachhang/create.php
@@ -10,10 +10,22 @@ include_once(__DIR__.'/../../dbconnect.php');
 if(isset($_POST['btnSave'])) 
 {
     // Lấy dữ liệu người dùng hiệu chỉnh gởi từ REQUEST POST
-    $tenHinhTTT = $_POST['httt_ten'];
-
+    $kh_tendangnhap = $_POST['kh_tendangnhap'];
+    $kh_matkhau = $_POST['kh_matkhau'];
+    $kh_ten = $_POST['kh_ten'];
+    $kh_gioitinh = $_POST['kh_gioitinh'];
+    $kh_diachi = $_POST['kh_diachi'];
+    $kh_dienthoai = $_POST['kh_dienthoai'];
+    $kh_email = $_POST['kh_email'];
+    $kh_ngaysinh = $_POST['kh_ngaysinh'];
+    $kh_thangsinh = $_POST['kh_thangsinh'];
+    $kh_namsinh = $_POST['kh_namsinh'];
+    $kh_cmnd = $_POST['kh_cmnd'];
+    $kh_makichhoat = $_POST['kh_makichhoat'];
+    $kh_trangthai = $_POST['kh_trangthai'];
+    $kh_quantri = $_POST['kh_quantri'];
     // Câu lệnh INSERT
-    $sql = "INSERT INTO `hinhthucthanhtoan` (httt_ten) VALUES ('" . $tenHinhTTT . "');";
+    $sql = "INSERT INTO `khachhang` (kh_tendangnhap,kh_matkhau,kh_ten,kh_gioitinh,kh_diachi,kh_dienthoai,kh_email,kh_ngaysinh,kh_thangsinh,kh_namsinh,kh_cmnd,kh_makichhoat,kh_trangthai,kh_quantri) VALUES ('$kh_tendangnhap' , '$kh_matkhau' , '$kh_ten' , $kh_gioitinh, '$kh_diachi' , '$kh_dienthoai, '$kh_email' , $kh_ngaysinh , $kh_thangsinh  ,$kh_namsinh , '$kh_cmnd', '$kh_makichhoat' , $kh_trangthai, $kh_quantri);";
 
     // Thực thi INSERT
     mysqli_query($conn, $sql);
@@ -30,4 +42,4 @@ if(isset($_POST['btnSave']))
 
 
 // Yêu cầu `Twig` vẽ giao diện được viết trong file `backend/loaisanpham/create.html.twig`
-echo $twig->render('backend/hinhthucthanhtoan/create.html.twig');
+echo $twig->render('backend/khachhang/create.html.twig');
--- frontend/index.php
+++ frontend/index.php
@@ -62,16 +62,114 @@ while($row = mysqli_fetch_array($resultDanhSachSanPhamLastest, MYSQLI_ASSOC))
     );
 }
 
+$sqlDanhSachSanPhamTopRate = <<<EOT
+        SELECT sp.sp_ma, sp.sp_ten, sp.sp_gia, lsp.lsp_ten, MAX(hsp.hsp_tentaptin) AS hsp_tentaptin
+        FROM `sanpham` sp
+        JOIN `loaisanpham` lsp ON sp.lsp_ma = lsp.lsp_ma
+        LEFT JOIN `hinhsanpham` hsp ON sp.sp_ma = hsp.sp_ma
+        GROUP BY sp.sp_ma, sp.sp_ten, sp.sp_gia, lsp.lsp_ten
+        ORDER BY sp.sp_gia desc
+        LIMIT  3
+
+EOT;
+// 3. Thực thi câu truy vấn SQL để lấy về dữ liệu
+$resultDanhSachSanPhamTopRate = mysqli_query($conn, $sqlDanhSachSanPhamTopRate);
+// 4. Khi thực thi các truy vấn dạng SELECT, dữ liệu lấy về cần phải phân tích để sử dụng
+// Thông thường, chúng ta sẽ sử dụng vòng lặp while để duyệt danh sách các dòng dữ liệu được SELECT
+// Ta sẽ tạo 1 mảng array để chứa các dữ liệu được trả về
+$dataDanhSachSanPhamTopRate = [];
+while($row = mysqli_fetch_array($resultDanhSachSanPhamTopRate, MYSQLI_ASSOC))
+{
+    $dataDanhSachSanPhamTopRate[] = array(
+        'sp_ma' => $row['sp_ma'],
+        'sp_ten' => $row['sp_ten'],
+        'sp_gia' => number_format($row['sp_gia'], 2, ".", ",") . ' vnđ',
+        'lsp_ten' => $row['lsp_ten'],
+        'hsp_tentaptin' => $row['hsp_tentaptin'],
+    );
+}
+
+$sqlDanhSachSanPhamReview = <<<EOT
+        SELECT sp.sp_ma, sp.sp_ten, sp.sp_gia, lsp.lsp_ten, MAX(hsp.hsp_tentaptin) AS hsp_tentaptin
+        FROM `sanpham` sp
+        JOIN `loaisanpham` lsp ON sp.lsp_ma = lsp.lsp_ma
+        LEFT JOIN `hinhsanpham` hsp ON sp.sp_ma = hsp.sp_ma
+        GROUP BY sp.sp_ma, sp.sp_ten, sp.sp_gia, lsp.lsp_ten
+        ORDER BY sp.sp_ma DESC
+        LIMIT  3
+
+EOT;
+// 3. Thực thi câu truy vấn SQL để lấy về dữ liệu
+$resultDanhSachSanPhamReview = mysqli_query($conn, $sqlDanhSachSanPhamReview);
+// 4. Khi thực thi các truy vấn dạng SELECT, dữ liệu lấy về cần phải phân tích để sử dụng
+// Thông thường, chúng ta sẽ sử dụng vòng lặp while để duyệt danh sách các dòng dữ liệu được SELECT
+// Ta sẽ tạo 1 mảng array để chứa các dữ liệu được trả về
+$dataDanhSachSanPhamReview = [];
+while($row = mysqli_fetch_array($resultDanhSachSanPhamReview, MYSQLI_ASSOC))
+{
+    $dataDanhSachSanPhamReview[] = array(
+        'sp_ma' => $row['sp_ma'],
+        'sp_ten' => $row['sp_ten'],
+        'sp_gia' => number_format($row['sp_gia'], 2, ".", ",") . ' vnđ',
+        'lsp_ten' => $row['lsp_ten'],
+        'hsp_tentaptin' => $row['hsp_tentaptin'],
+    );
+}
+
+$sqlDanhSachSanPhamCarousel= <<<EOT
+        SELECT sp.sp_ma, sp.sp_ten, lsp.lsp_ten, MAX(hsp.hsp_tentaptin) AS hsp_tentaptin
+        FROM `sanpham` sp
+        JOIN `loaisanpham` lsp ON sp.lsp_ma = lsp.lsp_ma
+        LEFT JOIN `hinhsanpham` hsp ON sp.sp_ma = hsp.sp_ma
+        GROUP BY sp.sp_ma, sp.sp_ten, sp.sp_gia, lsp.lsp_ten
+
+
+EOT;
+// 3. Thực thi câu truy vấn SQL để lấy về dữ liệu
+$resultDanhSachSanPhamCarousel = mysqli_query($conn, $sqlDanhSachSanPhamCarousel);
+// 4. Khi thực thi các truy vấn dạng SELECT, dữ liệu lấy về cần phải phân tích để sử dụng
+// Thông thường, chúng ta sẽ sử dụng vòng lặp while để duyệt danh sách các dòng dữ liệu được SELECT
+// Ta sẽ tạo 1 mảng array để chứa các dữ liệu được trả về
+$dataDanhSachSanPhamCarousel = [];
+while($row = mysqli_fetch_array($resultDanhSachSanPhamCarousel, MYSQLI_ASSOC))
+{
+    $dataDanhSachSanPhamCarousel[] = array(
+        'sp_ma' => $row['sp_ma'],
+        'sp_ten' => $row['sp_ten'],
+        'lsp_ten' => $row['lsp_ten'],
+        'hsp_tentaptin' => $row['hsp_tentaptin'],
+    );
+}
+
+$sqlDanhSachSanPhamDanhMuc= <<<EOT
+        SELECT distinct  lsp.lsp_ten
+        FROM `loaisanpham` lsp
 
 
 
+EOT;
+// 3. Thực thi câu truy vấn SQL để lấy về dữ liệu
+$resultDanhSachSanPhamDanhMuc = mysqli_query($conn, $sqlDanhSachSanPhamDanhMuc);
+// 4. Khi thực thi các truy vấn dạng SELECT, dữ liệu lấy về cần phải phân tích để sử dụng
+// Thông thường, chúng ta sẽ sử dụng vòng lặp while để duyệt danh sách các dòng dữ liệu được SELECT
+// Ta sẽ tạo 1 mảng array để chứa các dữ liệu được trả về
+$dataDanhSachSanPhamDanhMuc = [];
+while($row = mysqli_fetch_array($resultDanhSachSanPhamDanhMuc, MYSQLI_ASSOC))
+{
+    $dataDanhSachSanPhamDanhMuc[] = array(
+        'lsp_ten' => $row['lsp_ten'],
+    );
+}
 // var_dump($dataDanhSachSanPhamLastest);die;
 // print_r($dataDanhSachSanPham);die;
 // Yêu cầu `Twig` vẽ giao diện được viết trong file `frontend/pages/home.html.twig`
 // với dữ liệu truyền vào file giao diện được đặt tên
 echo $twig->render('frontend/pages/home.html.twig', [
-    'danhsachsanpham' => $dataDanhSachSanPham
-, 
-    'danhsachsanphamLastest' => $dataDanhSachSanPhamLastest
+    'danhsachsanpham' => $dataDanhSachSanPham, 
+    'danhsachsanphamLastest' => $dataDanhSachSanPhamLastest,
+    'danhsachsanphamTopRate' => $dataDanhSachSanPhamTopRate,
+    'danhsachsanphamReview' => $dataDanhSachSanPhamReview,
+    'danhsachsanphamCarousel' => $dataDanhSachSanPhamCarousel,
+    'danhsachsanphamDanhMuc' => $dataDanhSachSanPhamDanhMuc
 ]);
 ?>
